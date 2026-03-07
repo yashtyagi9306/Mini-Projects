24d346c66bcc1aebfaab8bc1c9092246dbec52e9
yashtyagi9306 <yashu66556655@gmail.com>
2026-03-07 11:51:32 +0530

    sound bar

---
+++ Sound_Player/script.js
@@ -0,0 +1,15 @@
+document.getElementById('sound1').addEventListener('click',function(){
+    const audio = new Audio("audio/1.mp3.mp3");
+    audio.play();
+});
+
+document.getElementById('sound2').addEventListener('click',function(){
+    const audio = new Audio("audio/2.mp3.mp3");
+    audio.play();
+});
+
+
+document.getElementById('sound3').addEventListener('click',function(){
+    const audio = new Audio("audio/3.mp3.mp3");
+    audio.play();
+});
