79facc6e511984df623a41608b8e539af06fedb9
yashtyagi9306 <yashu66556655@gmail.com>
2026-04-06 10:44:13 +0530

    learnt typed.js library

---
+++ auto_typing_tutorial/script..js
@@ -0,0 +1,18 @@
+const typed = new Typed('.auto-type',{
+    strings: ['Developer','Designer','Content Creator','Monk','Freelancer'],
+    typeSpeed:80,
+    backSpeed :80,
+    loop: true,
+    backDelay: 1000,
+    cursorChar: "|",
+    smartBackspace: true,
+
+});
+
+  var typed6 = new Typed('.auto-type-2', {
+    strings: ['npm install^1000\n `installing components...` ^1000\n `Fetching from source...`'],
+    typeSpeed: 80,
+    backSpeed: 0,
+    loop: true
+  });
+    
